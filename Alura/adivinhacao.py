d1f295235436c90149facf834b82060cae9cb3a8
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-02-07 11:50:38 -0300

    Usando a função abs()

--- Alura/adivinhacao.py
+++ Alura/adivinhacao.py
@@ -3,10 +3,9 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-
 numero_secreto = random.randint(1,100)
-
-total_de_tentativas = 3
+total_de_tentativas = 5
+pontos = 1000
 
 
 for rodada in range(1,total_de_tentativas+1):
@@ -24,11 +23,16 @@ for rodada in range(1,total_de_tentativas+1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Parabéns! Você acertou!")
+        print("Parabéns! Você acertoue fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
+            print()
             print("O seu chute foi maior do que o número secreto!")
         elif(menor):
+            print()
             print("O seu chute foi menor do que o número secreto!")
+            pontos_perdidos =abs(numero_secreto-chute) #Número absoluto, ele sempre será um valor positivo 
+            pontos = pontos - pontos_perdidos
+print()            
 print("Fim do jogo número era o ",numero_secreto)
