db24abfddfe2d8db6a27b625db6920e111db19ef
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-02-08 15:16:04 -0300

    usando laço e identificando posicao das letras

--- Alura/forca.py
+++ Alura/forca.py
@@ -8,6 +8,13 @@ def jogo_forca():
     acertou =False
     while (not enforcou and not acertou):
         print("Jogando...")
+        chute=input("Qual Letra: ").strip()
+
+        index =0
+        for letra in palavra_secreta:
+            if (chute.upper() ==letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index +=1
 
 
     print("Fim do jogo!!")
